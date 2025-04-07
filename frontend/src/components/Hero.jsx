47ae9362e4910835fdaa71114adf2567c231e40b
laavanya-2025 <Laavanyalaavanya2004@gmail.com>
2025-04-07 13:16:08 +0530

    changes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </div>
      </div>
       {/* HERO RIGHT side*/}
-      <img className='w-full sm:w-1/2 ' src={assets.hero_img} alt="" />
+      <img className='w-full sm:w-1/2 ' src={assets.hero_img1} alt="" />
 
 
     </div>
